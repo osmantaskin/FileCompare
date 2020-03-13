ab430ace06c2d52c5f8725681ade023660927243
osmantaskin <osmantaskin1@gmail.com>
2020-03-13 09:22:16 +0300

    Add project files.

---
+++ FileCompare/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FileCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKaynak = new System.Windows.Forms.TextBox();
+            this.txtHedef = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // txtKaynak
+            // 
+            this.txtKaynak.Location = new System.Drawing.Point(12, 12);
+            this.txtKaynak.Name = "txtKaynak";
+            this.txtKaynak.Size = new System.Drawing.Size(538, 20);
+            this.txtKaynak.TabIndex = 0;
+            this.txtKaynak.Text = "D:\\osman.taskin\\Masaüstü\\File Compare\\WorkFlowKWVNewTest";
+            // 
+            // txtHedef
+            // 
+            this.txtHedef.Location = new System.Drawing.Point(556, 12);
+            this.txtHedef.Name = "txtHedef";
+            this.txtHedef.Size = new System.Drawing.Size(538, 20);
+            this.txtHedef.TabIndex = 1;
+            this.txtHedef.Tag = "";
+            this.txtHedef.Text = "D:\\osman.taskin\\Masaüstü\\File Compare\\WorkFlowKWVNew";
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(12, 38);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(175, 23);
+            this.btnCompare.TabIndex = 2;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 67);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1082, 624);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Durumu";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Dosya Yolu";
+            this.columnHeader2.Width = 500;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Kaynak Tarih";
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Hedef Tarih";
+            this.columnHeader4.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 703);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.txtHedef);
+            this.Controls.Add(this.txtKaynak);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Compare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKaynak;
+        private System.Windows.Forms.TextBox txtHedef;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ FileCompare/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileCompare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<FileRead> ListKaynak = new List<FileRead>();
+        List<FileRead> ListHedef = new List<FileRead>();
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            ListKaynak = new List<FileRead>();
+            ListHedef = new List<FileRead>();
+
+            List<string> files = new List<string>();
+            files.AddRange(Directory.GetFiles(txtKaynak.Text, "*.*", SearchOption.AllDirectories));
+
+            foreach (var item in files)
+            {
+                FileRead fileRead = new FileRead();
+                fileRead.Path = item.Replace(txtKaynak.Text, "");
+                fileRead.FullPath = Path.GetFullPath(item);
+                fileRead.FileName = Path.GetFileName(item);
+                fileRead.CreationTime = File.GetCreationTime(item);
+                fileRead.LastWriteTime = File.GetLastWriteTime(item);
+                fileRead.LastWriteTimeString = File.GetLastWriteTime(item).ToString("dd.MM.yyyy HH:mm");
+                fileRead.Size = new System.IO.FileInfo(item).Length;
+                ListKaynak.Add(fileRead);
+            }
+
+
+            files = new List<string>();
+            files.AddRange(Directory.GetFiles(txtHedef.Text, "*.*", SearchOption.AllDirectories));
+
+            foreach (var item in files)
+            {
+                FileRead fileRead = new FileRead();
+                fileRead.Path = item.Replace(txtHedef.Text, "");
+                fileRead.FullPath = Path.GetFullPath(item);
+                fileRead.FileName = Path.GetFileName(item);
+                fileRead.CreationTime = File.GetCreationTime(item);
+                fileRead.LastWriteTime = File.GetLastWriteTime(item);
+                fileRead.LastWriteTimeString = File.GetLastWriteTime(item).ToString("dd.MM.yyyy HH:mm");
+                fileRead.Size = new System.IO.FileInfo(item).Length;
+                ListHedef.Add(fileRead);
+            }
+
+            CompareRun();
+        }
+
+        private void CompareRun()
+        {
+            listView1.Items.Clear();
+            foreach (FileRead item in ListKaynak)
+            {
+                FileRead fileReadHedef = ListHedef.Find(f => f.Path == item.Path);
+                if (fileReadHedef != null)//file varsa
+                {
+                    if (fileReadHedef.LastWriteTimeString != item.LastWriteTimeString || fileReadHedef.Size != item.Size)
+                    {
+                        ListViewItem li = new ListViewItem();
+                        li.Text = "Değişen";
+                        li.SubItems.Add(item.Path);
+                        li.SubItems.Add(item.LastWriteTime.ToString("dd.MM.yyyy HH:mm"));
+                        li.SubItems.Add(fileReadHedef.LastWriteTime.ToString("dd.MM.yyyy HH:mm"));
+                        li.ForeColor = System.Drawing.Color.Blue;
+                        listView1.Items.Add(li);
+                    }
+                }
+                else
+                {
+                    ListViewItem li = new ListViewItem();
+                    li.Text = "Dosya Yok";
+                    li.SubItems.Add(item.Path);
+                    li.SubItems.Add(item.LastWriteTime.ToString("dd.MM.yyyy HH:mm"));
+                    li.ForeColor = System.Drawing.Color.Green;
+                    listView1.Items.Add(li);
+                }
+            }
+
+            foreach (FileRead item in ListHedef)
+            {
+                FileRead fileReadKaynak = ListKaynak.Find(f => f.Path == item.Path);
+                if (fileReadKaynak != null)//file varsa
+                {
+                    //if (fileReadKaynak.LastWriteTime != item.LastWriteTime)
+                    //{
+                    //    listBox1.Items.Add("Değişen - " + item.Path);
+                    //}
+                }
+                else
+                {
+                    ListViewItem li = new ListViewItem();
+                    li.Text = "Dosya Fazla";
+                    li.SubItems.Add(item.Path);
+                    li.SubItems.Add(item.LastWriteTime.ToString("dd.MM.yyyy HH:mm"));
+                    li.ForeColor = System.Drawing.Color.Red;
+                    listView1.Items.Add(li);
+                }
+            }
+        }
+
+
+
+        public class FileRead
+        {
+            public string FileName { get; set; }
+            public string Path { get; set; }
+            public string FullPath { get; set; }
+            public long Size { get; set; }
+            public DateTime CreationTime { get; set; }
+            public DateTime LastWriteTime { get; set; }
+            public string LastWriteTimeString { get; set; }
+        }
+    }
+}
